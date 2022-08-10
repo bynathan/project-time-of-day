5282de3da8f142ab47d2c9b4c4d9ad348296ccfb
Nathan da Silva Lira <108292134+bynathan@users.noreply.github.com>
2022-08-10 15:09:33 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function load() {
+function load(){
     const msgDiv = document.getElementById('msg');
     const msgDiv2 = document.getElementById('msg2');
     const imgDiv = document.getElementById('img');
@@ -6,10 +6,33 @@ function load() {
     const favicon = document.getElementById('favicon')
     const daymsg = document.getElementById('daymsg')
     const photoalte = document.getElementById('photoalte')
+    const title = document.getElementById('title')
     let date = new Date();
     let hour = date.getHours();
-    let min = date.getMinutes()
-    let day = date.getDay()
+    let min = date.getMinutes();
+    let day = date.getDay();
+
+    if(min == 0){
+        min = "00"
+    } else if(min == 1){
+        min = "01"
+    } else if(min == 2){
+        min = "02"
+    } else if(min == 3){
+        min = "03"
+    } else if(min == 4){
+        min = "04"
+    } else if(min == 5){
+        min = "05"
+    } else if(min == 6){
+        min = "06"
+    } else if(min == 7){
+        min = "07"
+    } else if(min == 8){
+        min = "08"
+    } else if(min == 9){
+        min = "09"
+    }
 
     msgDiv.innerHTML = ` ↻ AGORA SÃO ${hour}:${min} HORAS ↺ `
 
@@ -36,26 +59,27 @@ function load() {
         photoalte.src = "./img/madruga-alte.png"
         document.body.style.background = "#002154"
         msgDiv2.innerHTML = "<p>~ TENHA UMA BOA MADRUGA ~</p>"
+        title.innerHTML = "~ Hora do Dia: Madrugada"
     } else if(hour >= 5 && hour <= 11) {
-        // MANHA
         favicon.setAttribute('href', './img/faviconmoning.png')
         photo.src = "./img/manhaphoto.png"
         photoalte.src = "./img/dia-alte.png"
         document.body.style.background = "#6BA9D8"
         msgDiv2.innerHTML = "<p>~ TENHA UM BOM DIA ~</p>"
+        title.innerHTML = "~ Hora do Dia: Manhã"
     } else if (hour >= 12 && hour <= 18) {
-        //TARDE
         favicon.setAttribute('href', './img/faviconafternoom.png')
         photo.src = "./img/tardephoto.png"
         photoalte.src = "./img/tarde-alte.png"
         document.body.style.background = "#E2B176"
         msgDiv2.innerHTML = "<p>~ TENHA UMA BOA TARDE ~</p>"
+        title.innerHTML = "~ Hora do Dia: Tarde"
     } else if (hour >= 19 && hour <= 23) {
-        //NOITE
         favicon.setAttribute('href', './img/faviconnight.png')
         photo.src = "./img/noitephoto.png"
         photoalte.src = "./img/noite-alte.png"
         document.body.style.background = "#130E14"
         msgDiv2.innerHTML = "<p>~ TENHA UMA BOA NOITE ~</p>"
+        title.innerHTML = "~ Hora do Dia: Noite"
     }
 }
